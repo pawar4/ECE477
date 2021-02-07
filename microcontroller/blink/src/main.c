7ee0d32f88d226d7f7f98ad31aab3961201e241a
ramacha7 <ramacha7@purdue.edu>
2021-02-07 13:49:31 -0500

    Avoiding merge issues

--- microcontroller/blink/src/main.c
+++ microcontroller/blink/src/main.c
@@ -11,10 +11,16 @@
 
 #include "stm32l1xx.h"
 #include "stm32l_discovery_lcd.h"
+#include "stm32l1xx_gpio.h"
+#include <stdio.h>
+#include <stdlib.h>
 			
 
 int main(void)
 {
+    RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB,ENABLE);
 
-	for(;;);
+    GPIO_InitTypeDef Init_Periph;
+    Init_Periph.GPIO_Pin = GPIO_Pin_7
+    
 }
