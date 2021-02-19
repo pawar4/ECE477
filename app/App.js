bfa83bcc07599939bd948fe6a98db8227d0ef2b6
pawar4 <pawar4@purdue.edu>
2021-02-19 15:41:49 -0500

    add bluetooth connection

--- app/App.js
+++ app/App.js
@@ -1,16 +1,125 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { Text, View } from 'react-native';
+import { NativeEventEmitter, NativeModules, Platform, PermissionsAndroid } from 'react-native';
+import { Text, View, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapView from "react-native-maps";
+import BleManager from 'react-native-ble-manager';
+
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
 
 function HomeScreen() {
+    const [isScanning, setIsScanning] = useState(false);
+    const peripherals = new Map();
+    const [list, setList] = useState([]);
+    
+    const startScan = () => {
+        if (!isScanning) {
+            BleManager.scan([], 3, true).then((results) => {
+            console.log('Scanning...');
+            setIsScanning(true);
+            }).catch(err => {
+            console.error(err);
+            });
+        }    
+    }
+
+    const handleStopScan = () => {
+        console.log('Scan is stopped');
+        setIsScanning(false);
+    }
+
+    const handleDiscoverPeripheral = (peripheral) => {
+        if (peripheral.name == 'LE Surface Headphones'){
+            console.log('Got ble peripheral', peripheral.id);
+            if (!peripheral.name) {
+            peripheral.name = 'NO NAME';
+            }
+            peripherals.set(peripheral.id, peripheral);
+            setList(Array.from(peripherals.values()));
+        }
+    }
+
+    const testPeripheral = (peripheral) => {
+        if (peripheral) {
+            if (peripheral.connected) {
+                BleManager.disconnect(peripheral.id);
+            }
+            else {
+                BleManager.connect(peripheral.id).then(() => {
+                    let p = peripherals.get(peripheral.id);
+                    if (p) {
+                        p.connected = true
+                        peripherals.set(peripheral.id, p);
+                        setList(Array.from(peripherals.values()));
+                    }
+                    console.log('Connected to ' + peripheral.id)
+                })
+            }
+                
+        }
+    }
+
+    const retrieveConnected = () => {
+        BleManager.getConnectedPeripherals([]).then((results) => {
+            if (results.length == 0) {
+                console.log('No connected peripherals')
+            }
+            console.log(results);
+            for (var i = 0; i < results.length; i++) {
+                var peripheral = results[i];
+                peripheral.connected = true;
+                peripherals.set(peripheral.id, peripheral);
+                setList(Array.from(peripherals.values()));
+            }
+        });
+    }
+
+    React.useEffect(() => {
+        // this is a constructor (runs once)
+        BleManager.start({ showAlert: false }).then(() => {
+        // Success code
+            console.log("Module initialized");
+        });
+        if (Platform.OS === 'android' && Platform.Version >= 23) {
+            PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
+                if (result) {
+                  console.log("Permission is OK");
+                } else {
+                  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
+                    if (result) {
+                      console.log("User accept");
+                    } else {
+                      console.log("User refuse");
+                    }
+                  });
+                }
+            });
+          }  
+        bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral);
+        bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan );
+    }, [])
+
     return (
         <View style={{  flex: 1,
                         justifyContent: 'center',
                         alignItems: 'center'}}>
-            <Text>Home</Text>
+            <View style={{margin: 10}}>
+                <Button 
+                    title={'Scan Bluetooth (' + (isScanning ? 'on' : 'off') + ')'}
+                    onPress={() => startScan() } 
+                />            
+            </View>
+
+            <View style={{margin: 10}}>
+                <Button title="Retrieve connected peripherals" onPress={() => retrieveConnected() } />
+            </View>
+
+            <View style={{margin: 10}}>
+                <Button title="Connect to peripheral" onPress={() => testPeripheral(list[0]) } />
+            </View>
         </View>
     );
 }
@@ -54,28 +163,11 @@ function MyTabs() {
 }
 
 export default function App() {
+
+
     return (
     <NavigationContainer>
         <MyTabs />
     </NavigationContainer>
     );
 }
-
-// const App = () => {
-//     const [region, setRegion] = useState({
-//         latitude: 51.5079145,
-//         longitude: -0.0899163,
-//         latitudeDelta: 0.01,
-//         longitudeDelta: 0.01 
-//     });
-
-//     return (
-//         <MapView 
-//             style={{  flex: 1  }} 
-//             region={region}
-//             onRegionChangeComplete={region => setRegion(region)}
-//         />
-//     );
-// }
-
-// export default App;
