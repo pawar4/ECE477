16bbae23f7761e5a48175fb2ed81bb417c412559
ramacha7 <ramacha7@purdue.edu>
2021-02-26 16:10:11 -0500

    Adding functional bluetooth sending and receving part of app to main branch

--- app/android/app/src/main/java/com/test/MainApplication 2.java
+++ app/android/app/src/main/java/com/test/MainApplication 2.java
@@ -1,11 +1,9 @@
 package com.test;
-
+import com.rusel.RCTBluetoothSerial.*;
 import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import io.github.douglasjunior.ReactNativeEasyBluetooth.classic.ClassicPackage;
-import io.github.douglasjunior.ReactNativeEasyBluetooth.classic.ClassicPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -27,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+          //packages.add(new RCTBluetoothSerialPackage());
+          
           return packages;
         }
 
