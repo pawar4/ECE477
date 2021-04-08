1cda2c5d2bb0f0baf124d40a43a3632e3de8a626
chibuikemohanele <chibuiohanele1@gmail.com>
2021-04-08 16:27:18 -0400

    GPS code

--- gps/src/main.c
+++ gps/src/main.c
@@ -19,53 +19,11 @@
 
 int main(void)
 {
-	GPIO_InitTypeDef  GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-	char receive_message[GPS_BUFFER_SIZE] = "";
+	initParsedMessage();
+	initUART2();
+	initTimer2();
 
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE); //Enabling GPIOA ports
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2 ,ENABLE); //Enabling USART1
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_40MHz;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
-	GPIO_Init(GPIOA, &GPIO_InitStructure);
-
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2);
-	GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2);
-
-	//USART settings
-	USART_InitStructure.USART_BaudRate = 9600;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-
-	USART_Init(USART2, &USART_InitStructure);     //Initialize UART settings
-
-	USART_Cmd(USART2, ENABLE);    //Enable UART
-	int i = 0;
-	char c = '\0';
-	nmea_message * parsed_message = calloc(1, sizeof(nmea_message));
-	parsed_message -> gll = calloc(1, sizeof(gll_message));
-	parsed_message -> gsv = calloc(1, sizeof(gsv_message));
-	parsed_message -> rmc = calloc(1, sizeof(rmc_message));
-	//char * begin_sentence;
 	while(1){
-		while(c != '\n'){
-			while(USART_GetFlagStatus(USART2, USART_FLAG_RXNE) == RESET);
-			receive_message[i] = USART_ReceiveData(USART2);
-			c = receive_message[i];
-			i++;
-		}
-		//begin_sentence = strstr(receive_message, "$");
-		parse_sentence(parsed_message, receive_message);
-		printf("%s\n", receive_message);
-		//free_nmea_message(parsed_message);
-		memset(receive_message, '\0', 82*sizeof(char));
-		c = '\0';
-		i = 0;
+
 	}
 }
--- gps/src/nmea.h
+++ gps/src/nmea.h
@@ -3,46 +3,49 @@
 #define GPS_BUFFER_SIZE 82
 
 typedef enum{
-	GLL, //Latitude and longitude
 	GSV, //Number of satellites
 	RMC,  //time and date
 	NONE
 }nmea_type;
 
 typedef struct{
-	char latitude[6];
-	char lat_direction;
-	char longitude[7];
-	char long_direction;
-	int valid;
-}gll_message;
-
-typedef struct{
 	int num_satellites;
 }gsv_message;
 
 typedef struct{
-	char time[9];
-	char latitude[14];
+	char time[10];
+	char latitude[25];
 	char lat_direction;
-	char longitude[15];
+	char longitude[25];
 	char long_direction;
 	char valid;
 }rmc_message;
 
 typedef struct{
-	nmea_type type;
-	gll_message * gll;
 	gsv_message * gsv;
 	rmc_message * rmc;
+	int init;
 }nmea_message;
 
-void parse_gll(gll_message * gll, char * buffer);
-void print_gll(gll_message * gll);
 void parse_gsv(gsv_message * gsv, char * buffer);
-void print_gsv(gsv_message * gsv);
 void parse_rmc(rmc_message * rmc, char * buffer);
-void print_rmc(rmc_message * rmc);
 nmea_type get_message_type(char * buffer);
-nmea_message * parse_sentence(nmea_message * message, char * buffer);
+void parse_sentence(nmea_message * message, char * buffer);
 void copyUntil(char * dest, char * source, char c);
+void psm_in();
+void psm_out();
+void convertLong(char * longitude);
+void convertLat(char * lat);
+void ftoa(float n, char* res, int afterpoint);
+int intToStr(int x, char str[], int d);
+void reverse(char* str, int len);
+void free_nmea_message(nmea_message * message);
+void mon_ver();
+void initUART2();
+void initTimer2();
+void initTimer3();
+void TIM2_IRQHandler();
+void initParsedMessage();
+void deinit_UART();
+void TIM3_IRQHandler();
+
